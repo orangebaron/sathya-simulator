9f67c0cf90fea97a02cee347fce613645c493e58
orangebaron <orangebaronroblox@gmail.com>
2017-10-31 13:23:52 -0400

    im actually pansexual

--- what-is-your-gender.php
+++ what-is-your-gender.php
@@ -6,11 +6,11 @@
 <body>
   <!-- wow this is my 10,000th line of code -->
 <form action='action="/come-out-of-the-closet.php'>
-waht fdood do yao want?!
+waht fdood do yao want?!<br>
 <input type="radio" name="gender" value="sandwich">yES,<br>
 <input type="radio" name="gender" value="pizza">boneless bizza<br>
 <input type="radio" name="gender" value="apple">give your soul to the devil<br>
-<input type='submit'>plaec lnuch ordere</input>
+<input type='submit' value='plaec lnuch ordere'></input>
 </form>
 </body>
 </html>
